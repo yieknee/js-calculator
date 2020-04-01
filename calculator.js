141ffef2c1af915b22f98738d298256afb01f9d5
Yieni <yaynee8d@gmail.com>
2020-03-31 18:51:31 -0700

    complete calculator

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const exampleAdditionInput = {
   operation: 'add',
 }
 
-const exampleAdditionInput = {
+const exampleAdditionInput2 = {
   num1: 3,
   num2: 5,
   operation: '+',
@@ -16,7 +16,7 @@ const exampleSubractionInput = {
   operation: 'subtract',
 }
 
-const exampleSubractionInput = {
+const exampleSubractionInput2 = {
   num1: 9,
   num2: 3,
   operation: '-',
@@ -28,7 +28,7 @@ const exampleMultiplicationInput = {
   operation: 'multiply',
 }
 
-const exampleMultiplicationInput = {
+const exampleMultiplicationInput2 = {
   num1: 12,
   num2: 23,
   operation: '*',
@@ -40,22 +40,95 @@ const exampleDivisionInput = {
   operation: 'divide',
 }
 
-const exampleDivisionInput = {
+const exampleDivisionInput2 = {
   num1: 13,
   num2: 4,
   operation: '/',
 }
 
-const validation = function(input){
+const exampleDiv0ErrorInput = {
+  num1: 13,
+  num2: 0,
+  operation: '/',
+}
+
+const exampleInvalidOperationInput = {
+  num1: 13,
+  num2: 4,
+  operation: 'quarantine',
+}
+
+const exampleInvalidNumInput = {
+  num1: 'horse',
+  num2: 23,
+  operation: '-',
+}
+
+const exampleInvalidNumInput2 = {
+  num1: 13,
+  num2: 'horse',
+  operation: '*',
+}
+
+const exampleInvalidNumInput3 = {
+  num1: 'horse',
+  num2: 'buggy',
+  operation: 'divide',
+}
+
+const calculate = function(input) { 
   if(isNaN(input.num1) || isNaN(input.num2)){
-    console.log('This is not a number. Please enter a number.')
+    if(isNaN(input.num1) && isNaN(input.num2)){
+      console.log(`${input.num1} and ${input.num2} are not numbers. Please enter numbers.`);
+    }
+    else if(isNaN(input.num1)){
+      console.log(`${input.num1} is not a numbers. Please enter a number.`);
+    }
+    else if(isNaN(input.num2)){
+      console.log(`${input.num2} is not a number. Please enter a number.`);
+    }
   }
-  else {
-    const calculation = function(input) { 
-      if(input.operation === 'add' || input.operation === '+'){
-        
+  else{
+
+    if(input.operation === 'add' || input.operation === '+'){
+      let sum = input.num1 + input.num2;
+      return sum;
+    }
+    else if(input.operation === 'subtract' || input.operation === '-'){
+      let difference = input.num1 - input.num2;
+      return difference;
+    }
+    else if(input.operation === 'multiply' || input.operation === '*'){
+      let product = input.num1 * input.num2;
+      return product;
+    }
+    else if(input.operation === 'divide' || input.operation === '/'){
+      if(input.num2 === 0){
+        console.log('Div/0 Error! You can not divide by 0!')
       }
+      else{
+        let quotient = input.num1 / input.num2;
+        return quotient;
+      }
+    }
+    else {
+      console.log(`${input.operation} is not a valid operand`);
     }
   }
 }
+
+console.log(calculate(exampleAdditionInput))
+console.log(calculate(exampleAdditionInput2))
+console.log(calculate(exampleSubractionInput))
+console.log(calculate(exampleSubractionInput2))
+console.log(calculate(exampleMultiplicationInput))
+console.log(calculate(exampleMultiplicationInput2))
+console.log(calculate(exampleDivisionInput))
+console.log(calculate(exampleDivisionInput2))
+console.log(calculate(exampleDiv0ErrorInput))
+console.log(calculate(exampleInvalidOperationInput))
+console.log(calculate(exampleInvalidNumInput))
+console.log(calculate(exampleInvalidNumInput2))
+console.log(calculate(exampleInvalidNumInput3))
+
 
